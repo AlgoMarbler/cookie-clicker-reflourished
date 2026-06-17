f4db2ecf0ca0d3c3b876a26b7b05887a86bb800e
AlgoMarbler <156500864+AlgoMarbler@users.noreply.github.com>
2026-06-18 09:54:34 +1200

    Update main.js

--- main.js
+++ main.js
@@ -14550,7 +14550,7 @@ Game.Launch=function()
 		new Game.buffType('golden surge', function(time, pow) {
 		    return {
 		        name: 'Golden surge',
-		        desc: loc("Golden cookies appear %1% more for %2!", [Math.round((pow - 1) * 100),Game.sayTime(time*Game.fps,-1)])),
+		        desc: loc("Golden cookies appear %1% more often for %2!", [(pow-1)*100, Game.sayTime(time*Game.fps,-1)]),
 		        icon: [22, 6],
 		        time: time * Game.fps,
 		        add: true
